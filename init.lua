8ab75c80b4694ac766ac59e0dee4ed8d140b7f06
rodgiee <rebecarodge@gmail.com>
2025-08-22 23:11:00 -0400

    added: packer, telescope, and remaps

---
+++ init.lua
@@ -0,0 +1,18 @@
+require("configurations")
+
+-- relative and number autoset
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- ensure netrw directory is synchronized with current working directory
+-- helps ensure telescope is looking at current working directory
+vim.g.netrw_keepdir = 0
+
+-- Enable relative numbers in netrw
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function()
+    vim.opt_local.relativenumber = true
+    vim.opt_local.number = true   -- keep absolute numbers too (optional)
+  end,
+})
