449059480fd1a7f17040126f7e7ed1457170c05d
rodgiee <rebecarodge@gmail.com>
2025-12-28 19:44:36 -0500

    added: see hidden files telescope

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -29,7 +29,7 @@ vim.api.nvim_set_keymap('t', '<C-w>N', [[<C-\><C-n>]], { noremap = true })
 
 -- telescope keybinds
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', function() builtin.find_files({hidden = true})end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
