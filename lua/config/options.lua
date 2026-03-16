15cf698e6923d1e65ba2a550c533a5027f200cd2
RodgeMarcus Rebeca <rodge.rebeca@macbookpro.lan>
2026-03-16 16:22:11 -0400

    add: tokyonight

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,7 +6,7 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 
 -- colorscheme
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("tokyonight-night")
 
 -- Enable relative numbers in netrw
 vim.api.nvim_create_autocmd("FileType", {
