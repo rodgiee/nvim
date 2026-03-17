9d2c12014d10573d2740f163a0847ee7827f624f
RodgeMarcus Rebeca <rodge.rebeca@macbookpro.lan>
2026-03-17 14:48:42 -0400

    add: swift lsp

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
 require("config.keymap")
 require("config.options")
+require("config.lsp")
 
---
+++ lua/config/lsp.lua
@@ -0,0 +1,12 @@
+-- sourcekit needed to be manually added since not supported by Mason
+-- vim.lsp.enable("sourcekit") will default use neovim/nvim-lspconfig version
+-- we want to use our local version here at lsp/sourcekit-revised.lua
+vim.lsp.enable("sourcekit-revised")
+
+ --vim.api.nvim_create_autocmd('LspAttach',{
+   --desc = 'LSP Actions',
+   --callback = function(args)
+     --vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap = true, silent = true})
+     --vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {noremap = true, silent = true})
+   --end
+ --})
